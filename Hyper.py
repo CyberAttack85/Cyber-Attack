53bf5d3e4b7f59e3124366ac336efd6b604f10d4
CyberAttack85 <153926827+CyberAttack85@users.noreply.github.com>
2023-12-16 10:38:11 +0500

    Update Hyper.py

--- Hyper.py
+++ Hyper.py
@@ -565,8 +565,7 @@ class crack_main():
                 print(' Cloning Is Started Wait For Results')
                 print(' After Every 5 Min Turn Airplane On/Off')
                 print(50*'-')
-                with tpd(max_workers=30) as saqi:
-			for user in id:
+                with tpd(max_workers=30) as saqi:			
 				uid,nm = user.split('|')
 				if 'i' in method:
 					saqi.submit(self.method1,uid,nm,pwx)
