e7f71df3a70a7e20fdb17b0357fe07bd40a4c2bc
CyberAttack85 <153926827+CyberAttack85@users.noreply.github.com>
2023-12-16 10:45:57 +0500

    Update Hyper.py

--- Hyper.py
+++ Hyper.py
@@ -583,7 +583,6 @@ class crack_main():
 "method": "auth.login",
 "fb_api_req_friendly_name": "authenticate",
 "fb_api_caller_class": "com.facebook.account.login.protocol.Fb4aAuthHandler",
-"api_key": "882a8490361da98702bf97a021ddc14d"}
 				headers = {'User-Agent': iAmAndroidUa(),
 'Content-Type': 'application/x-www-form-urlencoded',
 'Host': 'graph.facebook.com',
