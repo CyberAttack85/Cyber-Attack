f26f9a0c0213053cf423b087e98192ffa631891d
CyberAttack85 <153926827+CyberAttack85@users.noreply.github.com>
2023-12-17 10:47:01 +0500

    Update Cyber.py

--- Cyber.py
+++ Cyber.py
@@ -487,9 +487,34 @@ def rd(ids,passlist,fbcr):
 		time.sleep(20)
 	except Exception as e:
 		pass
-
-try:main()
-except requests.exceptions.ConnectionError:
-	print('\n No internet connection ...')
-except Exception as e:pass
-#rnd()
+def shoha_menu():
+    UMO="CYBER-"
+    uuid = str(os.geteuid()) + str(os.getlogin()) 
+    id = "5".join(uuid)
+    print(logo)
+    DARK=requests.get("https://github.com/U7P4L-IN/CRACK/blob/main/Appove.txt").text
+    if id in DARK:
+        CYBER()
+    else:
+        os.system("clear")
+        os.system("xdg-open https://t.me/ANONYMOUS_CYBER07")
+        time.sleep(3.0)
+        
+        os.system("clear")
+        print(logo)
+        print("\t\033[30m   [\033[1;32m\033[47m First Get Approvel\033[00m\033[1;30m]")
+        print ("")
+        print("┌━═━═━═━═━━═━═━═━═━═━═━═━━═━═━═━═━═━═━═━━═━═━═━═━═━═━═━┐ \n\033[1;32m│ Note : That is Paid because 100% ok id just now login│\033[1;37m\n└━═━═━═━═━━═━═━═━═━═━═━═━━═━═━═━═━═━═━═━━═━═━═━═━═━═━═━┘")
+        print ("")
+        print("                Your Key is Not Approved ")
+        print("               Copy And Send Key To Admin")
+        print ("")
+        print (" Your Key : "+UMO+id)
+        print ("\n")
+        name = input(" Your Name : ")
+        print ("")
+        input(" Press Enter To Send Key")
+        os.system("xdg-open https://t.me/ANONYMOUS_07XD")
+        shoha_menu()
+        
+shoha_menu()
