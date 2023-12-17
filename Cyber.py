c79909f8e153560aabf14ac39a3ccc05d38183ae
CyberAttack85 <153926827+CyberAttack85@users.noreply.github.com>
2023-12-17 08:09:20 +0500

    Update Cyber.py

--- Cyber.py
+++ Cyber.py
@@ -502,7 +502,7 @@ def superuser():
         
         os.system("clear")
         print(logo)
-        print("\t\033[30m   [\033[1;32m\033[47m First Get Approvel\033[00m\033[1;30m]")
+        print("   [First Get Approvel]   ")
         print ("")
         print("┌━═━═━═━═━━═━═━═━═━═━═━═━━═━═━═━═━═━═━═━━═━═━═━═━═━═━═━┐ \n\033[1;32m│ Note : That is Paid because 100% ok id just now login│\033[1;37m\n└━═━═━═━═━━═━═━═━═━═━═━═━━═━═━═━═━═━═━═━━═━═━═━═━═━═━═━┘")
         print ("")
@@ -513,7 +513,7 @@ def superuser():
         print ("\n")
         os.system("Assalamualaikum ,I am Jarvis VAI er  ROBOT and my boss is Umar.Sir this tool is paid because 100% ok id just now login\"")
         name = input(" Your Name : ")
-        os.system(f"espeak \"{name} ,prass Enter to send your key\"")
+        os.system("{name} ,prass Enter to send your key\"")
         print ("")
         input(" Press Enter To Send Key")
         os.system("xdg-open https://www.facebook.com/InnocentUmarr")
