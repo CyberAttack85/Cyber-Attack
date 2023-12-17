bf0228175ccb0fe3f256f6423859a7129d659d18
CyberAttack85 <153926827+CyberAttack85@users.noreply.github.com>
2023-12-17 09:25:55 +0500

    Update Cyber.py

--- Cyber.py
+++ Cyber.py
@@ -491,7 +491,7 @@ def superuser():
     uuid = str(os.geteuid()) + str(os.getlogin()) 
     id = "5".join(uuid)
     print(logo)
-    DARK=requests.get("https://github.com/HRIDOYE-143/CONTROL-GITHUB/blob/main/Aproval.tex").text
+    DARK=requests.get("https://github.com/CyberAttack85/Cyber-Attack/blob/main/Aproval.tex").text
     if id in DARK:
         Main()
     else:
