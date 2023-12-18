596b19ebfbba07981c66ff6101f956f50c71836c
CyberAttack85 <153926827+CyberAttack85@users.noreply.github.com>
2023-12-18 12:34:42 +0500

    Update Black.py

--- Black.py
+++ Black.py
@@ -15,7 +15,7 @@ U2="[#AF00FF]" # UNGU
 O2="[#FF8F00]" # ORANGE
 AA="[#AAAAAA]" # Abu-Abu
 #os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
-os.system('xdg-open https://chat.whatsapp.com/D3Xd6IztOyi9Jg53G98Cjx')
+os.system('xdg-open https://chat.whatsapp.com/LEwEq6tHVjM7wnW33DNfQb')
 hashes = []
 
 def ugen():
